0127c30d5976484381e999ac9cd90bc427630ba1
VladimirAnic <vladimiranic@gmail.com>
2017-09-19 10:24:44 +0200

    Hotfix added, signed apk

--- app/src/main/java/com/example/vladimir/firebasedemo/ChatMessage.java
+++ app/src/main/java/com/example/vladimir/firebasedemo/ChatMessage.java
@@ -19,8 +19,31 @@ public class ChatMessage {
         messageTime = new Date().getTime();
     }
 
+    public ChatMessage(){
+
+    }
+
     public String getMessageText() {
         return messageText;
     }
 
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
 }
